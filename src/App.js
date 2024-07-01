3e1598f91e24d98d48de2529fc373c1c2ea57000
Prasannakumar Kapate <prasanna.1616@gmail.com>
2024-07-01 13:39:29 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Menu/>
+      <Footer/>
+    </>
+  );
 }
 
 export default App;
